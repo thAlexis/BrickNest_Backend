4ef6a022ea3d549aee13844c3258989b1b72c6da
Alexis Th <thullier.alexis@hotmail.fr>
2025-09-30 10:35:07 +0200

    add index.js with base configuration

---
+++ src/index.js
@@ -0,0 +1,24 @@
+///////////// Configuration imports //////////////
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+/////////// Configuration //////////////
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+  })
+);
+
+////////// Local Host Configuration //////////
+const PORT = process.env.PORT || 5555;
+
+app.listen(PORT, () => {
+  console.log(`Adresse serveur : http://localhost:${PORT}`);
+});
