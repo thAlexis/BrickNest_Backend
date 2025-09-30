a49d1c86b5868b372bed3e0326329f97f9d76e7d
Alexis Th <thullier.alexis@hotmail.fr>
2025-09-30 11:51:35 +0200

    Add bcrypt config file

---
+++ src/config/bcrypt.config.js
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+
+async function hashPassword(password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+}
+
+async function verifyPassword(password, hashedPassword) {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+}
+
+export default { hashPassword, verifyPassword };
