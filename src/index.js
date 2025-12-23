ec6d08a42f35bdc14e7f9a498eeaa767b3df9823
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-12-23 11:07:50 +0100

    change cors

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Accept", "Authorization"],
   })
